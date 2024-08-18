e1f6e5f6b7dca4cc7526e02145a7008a81e87e8d
Gideaomc <83554956+Gideaomc@users.noreply.github.com>
2024-08-18 11:49:30 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const videoPlayer = document.getElementById('videoPlayer');
+    // Adicione funcionalidades extras aqui, se necessário
+});
